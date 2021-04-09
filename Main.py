173d7c81733f97f0d95740e98174de64e5e48306
Shubham Anil Kini <shubham144477@gmail.com>
2021-04-09 13:19:56 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
 #main
+from PyQt5 import QtGui, QtWidgets, QtCore
+from GUI import Ui_MainWindow
+import sys
+
+#This Runs first...
+class MyWork(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MyWork, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)      #Calls GUI.py, loads the GUI
+        self.show()
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = MyWork()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
